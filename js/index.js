ee43c9f7ee98b1eb3f434c5dc0cff45de91908e5
alialaba <alialaba079@gmail.com>
2021-11-17 17:08:11 -0800

    implemented tab switch functionality

--- js/index.js
+++ js/index.js
@@ -11,8 +11,25 @@ btnNavEl.addEventListener('click', () => {
 
 //TABS
 
-const openTab=(evt, tabName)=>{
-    let i, tab ,tabcontent;
-    
-
+const openTab = (evt, tabName) => {
+    let i, tabs, tabcontents;
+    tabcontents = document.getElementsByClassName("tab-content");
+    for (i = 0; i < tabcontents.length; i++) {
+        // console.log(tabcontent)
+        tabcontents[i].style.display = "none";
+    }
+    tabs = document.getElementsByClassName("tab");
+    for (i = 0; i < tabs.length; i++) {
+        tabs[i].className = tabs[i].className.replace(" active", "")
+    }
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
 }
+
+
+window.addEventListener("load", (event) => {
+    // event.preventDefault();
+    document.getElementById("tools-1").style.display = "block";
+    document.getElementById("tools-2").style.display = "none";
+    document.getElementById("tools-3").style.display = "none";
+})
